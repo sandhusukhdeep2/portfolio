5121c583a434076c26b7baaa5853b7f42d44d6e7
Sukhdeep Sandhu <sandhusukhdeep2@gmail.com>
2023-01-29 19:43:21 +1100

    Start Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png' 
 
 const Header = () => {
   return (
-    <div>Header is good</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Sukhdeep Sandhu</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <div className='me'>
+          <img src={ME} alt="sukhdeep"/>
+        </div>
+        <a href="#contact" className="scroll__down">Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href="https://www.linkedin.com/in/sukhdeep-singh-sandhu-a2abb94b/" target="__blank"><BsLinkedin/></a>
+      <a href="https://github.com/sandhusukhdeep2" target="__blank"></a>
+      <a href="https://www.linkedin.com/in/sukhdeep-singh-sandhu-a2abb94b/" target="__blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
