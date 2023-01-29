3f437dd9fa07c5be9d1f01ec7840a92b8df3470f
Sukhdeep Sandhu <sandhusukhdeep2@gmail.com>
2023-01-29 14:20:22 +1100

    add assets

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Contact from './components/contact/Contact'
+import About from './components/about/About'
+
+const app = () => {
+  return (
+    <>
+      <Header/>
+      <About/>
+      <Contact/>
+    </>
+  )
+}
+
+export default app
